4258f37eb990258fa8864de4f4acbe246f046f3c
shah-jahan <sazuctg007@gmail.com>
2024-02-05 00:26:05 +0600

    update

--- src/components/Newarrivals.jsx
+++ src/components/Newarrivals.jsx
@@ -1,20 +1,63 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import Container from './Container'
 import Flex from './Flex'
-import Newone from "../assets/newone.png"
+import Slider from "react-slick";
 import { FaCartPlus,FaHeart  } from "react-icons/fa";
 import { FaCodeCompare } from "react-icons/fa6";
+import axios from 'axios';
+import { MdOutlineArrowBackIosNew,MdArrowForwardIos  } from "react-icons/md";
+function SampleNextArrow(props) {
+    const { style, onClick } = props;
+    return (
+      <div
+        style={{position: 'absolute', right: '-10px', top: '150px', height: '40px', width: '40px', borderRadius:'50%', background:'#979797', zIndex:'999', transform:'-50', translate:'-50', lineHeight:'40px !', textAlign:'center !', fontSize:'35px', color:'white'}}
+        onClick={onClick}
+      ><MdArrowForwardIos className=''/> </div>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { style, onClick } = props;
+    return (
+      <div
+        // className={className}
+        style={{position: 'absolute', left: '-25px', top: '150px', height: '40px', width: '40px', borderRadius:'50%', background:'#979797', zIndex:'999', transform:'-50', translate:'-50', lineHeight:'0px !',  textAlign:'center !', fontSize:'35px', color:'white'}}
+        onClick={onClick}
+      ><MdOutlineArrowBackIosNew className=''/></div>
+    );
+  }
 
 const Newarrivals = () => {
-  return (
+    var settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+      };
+    let [info, setInfo] = useState([])
+
+    useEffect(()=>{
+        let getdata = () =>{
+            axios.get("https://dummyjson.com/products").then((respone)=>{
+                setInfo(respone.data.products);
+            })
+        }
+        getdata()
+    },[])
+
+    return (
     <div className='py-10'>
         <Container>
             <h2 className='font-dm font-bold text-[40px] text-[#262626]'>New Arrival</h2>
-            <Flex>
-                <div className="w-[24%]">
+                <Slider {...settings}> 
+                    {info.map((item)=>(
+                <div className='!w-[95%]'>
                     <div className="py-3">
                     <div className="relative group">
-                    <img src={Newone} alt="" />
+                    <img src={item.thumbnail} className='h-[300px] w-full' alt="" />
                     <div className="absolute bottom-0 overflow-y-hidden left-0 w-full h-[0px] opacity-0 duration-300 ease-in bg-[#fff] group-hover:h-[200px]  group-hover:opacity-100">
                         <div className="flex justify-end items-center h-full ">
                             <ul className="px-6 text-end">
@@ -26,12 +69,13 @@ const Newarrivals = () => {
                     </div>
                     </div>
                     <div className="flex justify-between pt-3">
-                        <h3 className='font-dm font-bold text-[16px] text-[#262626]'>Basic Cr    ew Neck Tee</h3>
-                        <p className='font-dm font-bold text-[16px] text-[#262626]'>$44.00</p>
+                        <h3 className='font-dm font-bold text-[16px] text-[#262626]'>{item.title}</h3>
+                        <p className='font-dm font-bold text-[16px] text-[#262626]'>{item.price}</p>
                     </div>
                     </div>
                 </div>
-            </Flex>
+                   ))}
+                   </Slider>
         </Container>
     </div>
   )
