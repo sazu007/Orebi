b1c0ce5407e8526ed7d696d1171a3e6a43b6df48
shah-jahan <sazuctg007@gmail.com>
2024-02-26 23:55:06 +0600

    update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState,useEffect,catOne,catTwo,showTab,showOne,setShowTabOne} from 'react'
 import Container from '../components/Container'
 import { Link } from 'react-router-dom'
 import Flex from '../components/Flex'
-// import { FaHeart,FaCartShopping } from "react-icons/fa6";
-// import { IoGitCompare } from "react-icons/io5";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+import { TiArrowSortedDown,TiArrowSortedUp } from "react-icons/ti";
 import { Apidata } from '../components/ContextApi'
 import Post from '../components/pagination/Post'
 import PaginationNum from '../components/pagination/PaginationNum'
@@ -13,6 +13,24 @@ const Product = () => {
     let data = useContext(Apidata)
     let [currentPage, setCurrentPage] = useState(1)
     let [perPage, setPerPage] = useState(6)
+
+    let [showIcon, setShowIcon] = useState(false)
+    let [show, setShow] = useState(false)
+    let catOne = useRef()
+
+    let [showTab, setShowTab] = useState(false)
+    let [showOne, setShowOne] = useState(false)
+    let catTwo = useRef()
+
+    let [showTabOne, setShowTabOne] = useState(false)
+    let [showTwo, setShowTwo] = useState(false)
+    let catThree = useRef()
+
+    let [showPrice, setShowPrice] = useState(false)
+    let [showThree, setShowThree] = useState(false)
+    let catFour = useRef()
+
+
 
     let pageNumber = []
     for(let i = 0; i < Math.ceil(data.length / perPage); i++) {
@@ -42,6 +60,59 @@ const Product = () => {
       }
     }
 
+    useEffect(() =>{
+document.addEventListener("click",(e)=>{
+  if(catOne.current.contains(e.target) == true){
+    setShow(!show)
+    setShowIcon(!showIcon)
+  }else{
+    setShow(false)
+    setShowIcon(!showIcon)
+    setShowIcon(false)
+  }
+})
+    },[show])
+
+
+    useEffect(() =>{
+      document.addEventListener("click",(e)=>{
+        if(catTwo.current.contains(e.target) == true){
+          setShowOne(!showOne)
+          setShowTab(!showTab)
+        }else{
+          setShowOne(false)
+          setShowTab(!showTab)
+          setShowTab(false)
+        }
+      })
+          },[showOne])
+
+    useEffect(() =>{
+      document.addEventListener("click",(e)=>{
+        if(catThree.current.contains(e.target) == true){
+          setShowTwo(!showTwo)
+          setShowTabOne(!showTabOne)
+        }else{
+          setShowTwo(false)
+          setShowTabOne(!showTabOne)
+          setShowTabOne(false)
+        }
+      })
+          },[showTwo])
+
+    useEffect(() =>{
+      document.addEventListener("click",(e)=>{
+        if(catFour.current.contains(e.target) == true){
+          setShowThree(!showThree)
+          setShowPrice(!showPrice)
+        }else{
+          setShowThree(false)
+          setShowPrice(!showPrice)
+          setShowPrice(false)
+        }
+      })
+          },[showThree])
+
   return (
     <div>
        <Container>
@@ -49,7 +120,67 @@ const Product = () => {
        
        <Flex>
       <div className="w-[20%]">
-        <h2>Under Construction</h2>
+
+        <div className="">
+          <h2 className="font-dm font-bold text-[18px] pt-3">Shop by Category</h2>
+
+          <div ref={catOne} className="flex justify-between items-center px-2">
+            <h4 className=' font-dm font-normal text-[18px] pt-2'>Category-1 </h4>
+            {showIcon ? <FaMinus /> : <FaPlus />}
+            </div>
+            {show &&  <ul className='bg-[gray] py-4 px-2'>
+              <li className=' font-dm font-normal text-[18px] py-4'>Catagory-2</li>
+              <li className=' font-dm font-normal text-[18px] py-4'>Catagory-3</li>
+              <li className=' font-dm font-normal text-[18px] py-4'>Catagory-4</li>
+              <li className=' font-dm font-normal text-[18px] py-4'>Catagory-5</li>
+            </ul>}
+           
+        </div>
+
+        <div className="">
+          <div ref={catTwo} className="flex justify-between items-center px-4 ">
+            <h4 className='font-dm font-normal text-[18px] pt-4'>Shop by Color</h4>
+            {showTab ? <TiArrowSortedDown className='text-[22px]'/> : <TiArrowSortedUp className='text-[22px]'/>}
+            </div>
+            {showOne && <ul>
+              <li className='font-dm font-normal text-[18px] py-4'>Color-1</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Color-2</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Color-3</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Color-4</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Color-5</li>
+            </ul>}
+        </div>
+
+        <div className="">
+          <div ref={catThree} className="flex justify-between items-center px-4 ">
+            <h4 className='font-dm font-normal text-[18px] pt-4'>Shop by Brand</h4>
+            {showTabOne ? <TiArrowSortedDown className='text-[22px]'/> : <TiArrowSortedUp className='text-[22px]'/>}
+            </div>
+            {showTwo && <ul>
+              <li className='font-dm font-normal text-[18px] py-4'>Brand-1</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Brand-2</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Brand-3</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Brand-4</li>
+              <li className='font-dm font-normal text-[18px] py-4'>Brand-5</li>
+            </ul>}
+        </div>
+
+        <div className="">
+          <div ref={catFour} className="flex justify-between items-center px-4 ">
+            <h4 className='font-dm font-normal text-[18px] pt-4'>Shop by Price</h4>
+            {showPrice ? <TiArrowSortedDown className='text-[22px]'/> : <TiArrowSortedUp className='text-[22px]'/>}
+            </div>
+            {showThree && <ul>
+              <li className='font-dm font-normal text-[18px] py-4'>$0.00 - $9.99</li>
+              <li className='font-dm font-normal text-[18px] py-4'>$10.00 - $19.99</li>
+              <li className='font-dm font-normal text-[18px] py-4'>$20.00 - $29.99</li>
+              <li className='font-dm font-normal text-[18px] py-4'>$30.00 - $39.99</li>
+              <li className='font-dm font-normal text-[18px] py-4'>$40.00 - $69.99</li>
+            </ul>}
+        </div>
+
+
+
         </div>
       <div className="w-[80%]">
         <div className="flex justify-between flex-wrap">
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Container from '../components/Container';
 import Flex from '../components/Flex';
+import { IoIosStar,IoIosStarHalf  } from "react-icons/io";
 
 
 const ProductDetails = () => {
@@ -30,9 +31,23 @@ const ProductDetails = () => {
         <div className="w-[32%"> 
     <img src={item} alt="" className='w-full' />
     </div>
-
 ))}
+
 </Flex> 
+<div className="">
+    <h2 className='font-dm font-normal text-[34px] text-[#262626]'>Product</h2>
+    <div className="">  
+    <div className="flex">
+    <IoIosStar />
+    <IoIosStar />
+    <IoIosStar />
+    <IoIosStar />
+    <IoIosStarHalf />
+    </div>
+    <span>Review</span>
+        <h4 className='font-dm font- text-[34px] text-[#262626]'>$44.00</h4>
+    </div>
+</div>
 </Container>
 </>
 
